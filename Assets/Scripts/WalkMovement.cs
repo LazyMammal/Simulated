15b513d4aa4284ec3e3f330d62c5d80a684e8cd7
Dennis Carlstrom <lazy@lazymammal.com>
2018-03-17 14:27:10 -0400

    camera movement

---
+++ Assets/Scripts/WalkMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WalkMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float inputForward = Input.GetAxis("Vertical");
+        float inputRight = -Input.GetAxis("Horizontal");
+
+        Vector3 planeForward = transform.forward;
+        planeForward.y = 0f;
+        planeForward.Normalize();
+        Vector3 planeRight = new Vector3(-planeForward.z, 0f, planeForward.x);
+
+        Vector3 moveVec = planeForward * inputForward + planeRight * inputRight;
+
+        Vector3 pos = transform.position;
+        pos += moveVec * speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
